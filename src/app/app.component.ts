9266648a2e8eab5f5675a06d43754a2633816972
zousd <zousd@baihe.com>
2019-04-12 18:11:46 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+//这里从Angular核心模块中引入了component装饰器
 import { Component } from '@angular/core';
 
+//用装饰器定义了一个组件以及组件的元数据，所有的组件都必须使用这个装饰器来注解
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  /**
+   * 组件元数据 Angular会通过这里的属性来渲染组件并执行逻辑
+   */
+  selector: 'app-root',//selector就是css选择器,表示这个组件可以通过app-root来调用,index.html中有个<app-root>Loading...</app-root>标签，这个标签用来展示该组件的内容
+  templateUrl: './app.component.html',//组件的模板，定义了组件的布局和内容
+  styleUrls: ['./app.component.css']//该模板引用哪个css样式
 })
+/**
+ * AppComponent本身就是一个普通的TypeScript类，但是上面的组件元数据装饰器告诉Angular,AppComponent是一个组件，
+ * 需要把一些元数据附加到这个类上，Angular就会把AppComponent当组件来处理
+ */
 export class AppComponent {
-  title = 'my-app';
+  //这个类实际上就是该组件的控制器，我们的业务逻辑就在这个类中编写
+  title = '学习Angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+//这个文件是Angular根模块，告诉Angular如何组装应用
+
+//BrowserModule ,浏览器解析的模块
 import { BrowserModule } from '@angular/platform-browser';
+
+//Angular 核心模块
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';//根组件
 
+//@NgModule装饰器,接收一个元数据对象，告诉Angular如何编译和启动应用
 @NgModule({
-  declarations: [
+  declarations: [//配置当前项目运行的组件(声明模块里有什么东西 只能声明：组件/指令/管道)
     AppComponent
   ],
-  imports: [
+  imports: [//配置当前模块运行依赖的其他模块// 声明该模块所依赖的模块
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//配置项目所需的服务
+  bootstrap: [AppComponent]//指定应用的主视图(称为根组件) 通过引导根AppModule来启动应用,这里一般写的是根组件// 声明模块的主组件是什么
 })
+
+//根模块不需要导出任何东西，因为其他模块不需要导入根模块
 export class AppModule { }
